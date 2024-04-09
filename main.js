8573dabfe0a4bc9c5374734e4155891ce6b7faaf
anarotgo <anarotgo@yahoo.com>
2024-04-08 19:46:03 -0700

    First commit copied from Portfolio Website

---
+++ main.js
@@ -0,0 +1,60 @@
+// Handle custom cursor
+document.addEventListener('DOMContentLoaded', function() {
+    const customCursor = document.getElementById('custom-cursor');
+    document.body.style.cursor = 'none'; // Hide the default cursor
+    const pointerElements = document.querySelectorAll('button, a, svg'); // Target buttons and links as an example
+    const textElements = document.querySelectorAll('h1, h2, h3, h4, li, p, span'); // Target paragraph texts
+
+    document.addEventListener('mousemove', function(e) {
+        // Update the position of the custom cursor element
+        customCursor.style.left = `${e.clientX}px`;
+        customCursor.style.top = `${e.clientY}px`;
+    });
+
+    // Show the custom cursor when the mouse enters the webpage
+    document.addEventListener('mouseenter', function() {
+        customCursor.style.display = 'block';
+    });
+
+    // Hide the custom cursor when the mouse leaves the webpage
+    document.addEventListener('mouseleave', function() {
+        customCursor.style.display = 'none';
+    });
+
+    pointerElements.forEach(elem => {
+        elem.addEventListener('mouseenter', () => customCursor.classList.add('pointer'));
+        elem.addEventListener('mouseleave', () => customCursor.classList.remove('pointer'));
+    });
+    textElements.forEach(elem => {
+        elem.addEventListener('mouseenter', () => customCursor.classList.add('text'));
+        elem.addEventListener('mouseleave', () => customCursor.classList.remove('text'));
+    })
+
+    // Handle navigation bar buttons:
+    document.querySelectorAll('.button').forEach(button => {
+        button.addEventListener('click', function(e) {
+            e.preventDefault(); // Prevent default behavior
+            const targetId = e.target.getAttribute('data-target');
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                // Calculate the position to scroll to
+                const offsetTop = targetElement.offsetTop - 128; // Offset for nav bar
+                window.scrollTo({
+                    top: offsetTop, behavior: 'smooth'
+                });
+            }
+        })
+    })
+
+    // Play project cover videos when hovered
+    document.querySelectorAll('.project-card').forEach(card => {
+        const video = card.querySelector('.image-cover');
+        card.addEventListener('mouseenter', () => {
+            video.play();
+        });
+        card.addEventListener('mouseleave', () => {
+            video.pause();
+        });
+    });
+});
